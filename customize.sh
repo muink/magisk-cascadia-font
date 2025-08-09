767b1c91e64a01bfadc1d03c9426fbf9f68b3ac9
Anya Lin <hukk1996@gmail.com>
2025-08-09 21:02:43 +0800

    v2407.24

--- customize.sh
+++ customize.sh
@@ -7,29 +7,19 @@ ui_print "      Magisk Cascadia Font     "
 ui_print "*******************************"
 
 ui_print "- Extracting module files"
-unzip -o "$ZIPFILE" -x 'META-INF/*' fonts.tar.xz -d $MODPATH >&2
+unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 # --------------------------------------------
 ui_print "- Searching in fonts.xml"
 [[ -d /sbin/.magisk/mirror ]] && MIRRORPATH=/sbin/.magisk/mirror || unset MIRRORPATH
-FILEPATH=/system/etc
-FILE=fonts.xml
-mkdir -p $MODPATH/$FILEPATH 2>/dev/null
-
-ui_print "- Unzipping font files..."
-FONTSPATH=/system/fonts
-zipinfo -1 "$ZIPFILE" >/dev/null && (
-  unzip -oj "$ZIPFILE" fonts.tar.xz -d $TMPDIR >&2
-  mkdir -p $MODPATH/$FONTSPATH 2>/dev/null
-  tar -xf $TMPDIR/fonts.tar.xz -C $MODPATH/$FONTSPATH 2>/dev/null
-)
+FILEPATH=/system/etc/fonts.xml
 
 ui_print "- Installing fonts..."
-RAWFONTS=`sed -En '/<family name="monospace">/,/<\/family>/ {s|.*<font weight="400" style="normal">(.*).ttf<\/font>.*|\1|p}' $MIRRORPATH/$FILEPATH/$FILE`
-NEWFONTS='CascadiaCode'
-#NEWFONTS='CascadiaMono'
+TARGET=`sed -En '/<family name="monospace">/,/<\/family>/ {s|.*<font weight="400" style="normal">(.*).ttf<\/font>.*|\1|p}' $FILEPATH`
+SOURCE='CascadiaCode'
+#SOURCE='CascadiaMono'
 
 # Just replace
-ln -s $FONTSPATH/${NEWFONTS}.ttf $MODPATH/$FONTSPATH/${RAWFONTS}.ttf
+ln -s /system/fonts/${SOURCE}.ttf $MODPATH/system/fonts/${TARGET}.ttf
 
 
 # Default permissions
